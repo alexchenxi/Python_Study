a53aa390df54b3b827e22fb4daeb0e399911615d
alex-zhang_1701 <alex.zhang@take2games.com>
2025-10-11 17:58:18 +0800

    update

---
+++ first-project/from_b_to_pro/chapter_2/section_3/2_3_1.py
@@ -0,0 +1,4 @@
+name = "hello world!"
+print(name.title())
+message = "THIS IS A LONG MESSAGE"
+print(message.lower())
---
+++ first-project/from_b_to_pro/chapter_2/section_3/2_3_2.py
@@ -0,0 +1,6 @@
+# 字符串中使用变量
+first_name="alex"
+last_name="zhang"
+
+full_name=f"{first_name} {last_name}".title()
+print(f"Hello, {full_name}!")
