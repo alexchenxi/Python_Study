a8385ef4add8a7c03c4c94552d91d6eaf75ed6ef
alex-zhang_1701 <alex.zhang@take2games.com>
2025-11-19 18:12:16 +0800

    up

---
+++ first-project/from_b_to_pro/chapter_8/section_1/practice.py
@@ -0,0 +1,14 @@
+# 8.1
+def display_message():
+    print("The topic of this chapter is Function.")
+
+
+display_message()
+
+
+# 8.2
+def favorite_book(title):
+    print(f"One of my favorite books is <<{title.title()}>>.")
+
+
+favorite_book("GOne with the wind")
