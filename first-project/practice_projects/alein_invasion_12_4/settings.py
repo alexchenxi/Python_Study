dad7265b6413bd8eda448ef19d2c03ef37c3b92c
Alex.Zhang <alexchenxizhang@163.com>
2025-12-28 22:22:45 +0800

    update

---
+++ first-project/practice_projects/alein_invasion_12_4/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """存储游戏的所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 1280
+        self.screen_height = 960
+        self.bg_color = (135, 206, 235)
+        self.ship_speed = 1
---
+++ first-project/practice_projects/alein_invasion_12_4/ship.py
@@ -0,0 +1,48 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        """初始化飞船并设置其初始位置"""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像并调整大小为80x64
+        self.image = pygame.image.load("images/ship.jpeg")
+        self.image = pygame.transform.scale(self.image, (80, 64))  # 缩放图像
+        self.rect = self.image.get_rect()
+        self.bg_color = ai_game.settings.bg_color
+
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # 在飞船的属性 x 中存储一个浮点数
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+        # 移动速度
+        self.moving_speed = self.settings.ship_speed
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.moving_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.moving_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.moving_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.moving_speed
+
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
